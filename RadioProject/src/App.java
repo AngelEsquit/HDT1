5c38f4b54985a1324dce01f9ecc7a8fe74803878
JavierEspana23361 <esp23361@uvg.edu.gt>
2024-01-23 08:08:29 -0600

    debbuging

--- RadioProject/src/App.java
+++ RadioProject/src/App.java
@@ -1,58 +1,8 @@
 package src;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Radio radio = new Radio();
-        int button;
-        int option = scanner.nextInt();
-        while (true){
-            switch (option) {
-                case 1:
-                    System.out.println("AM/FM");
-                    radio.toogleAMFM();
-                    break;
-                case 2:
-                    System.out.println("Siguiente emisora");
-                    radio.nextFrequency();
-                    break;
-                case 3:
-                    System.out.println("Anterior emisora");
-                    radio.previousFrequency();
-                    break;
-                case 4:
-                    System.out.println("Guardar emisora");
-                    System.out.println("Ingrese el número de botón en el cual desea guardar la emisora (del 1 al 12)");
-                    button = scanner.nextInt();
-                    if (button < 1 || button > 12) {
-                        System.out.println("Opción inválida");
-                        break;
-                    }
-                    else {
-                        radio.setFavFrequency(button);
-                        break;
-                    }
-                case 5:
-                    System.out.println("Seleccionar una emisora guardada");
-                    System.out.println("Ingrese el número de botón en el cual desea guardar la emisora (del 1 al 12)");
-                    button = scanner.nextInt();
-                    if (button < 1 || button > 12) {
-                        System.out.println("Opción inválida");
-                        break;
-                    }
-                    else {
-                        radio.getFavFrequency(button);
-                        break;
-                    }
-                case 6:
-                    System.out.println("Encender/Apagar la radio");
-                    radio.tooglePowerOffOn();
-                    break;
-                default:
-                    System.out.println("Opción inválida");
-                    break;
-            }
-        } 
+        MenuUI menu = new MenuUI();
+        menu.Menu();
     }
 }
--- RadioProject/src/MenuUI.java
+++ RadioProject/src/MenuUI.java
@@ -1,18 +1,72 @@
 package src;
+import java.util.Scanner;
 
 public class MenuUI {
-    public static void printMenu() {
-        System.out.println("");
-        System.out.println("**************************************");
-        System.out.println("                Menú");
-        System.out.println("**************************************");
-        System.out.println("Ingrese la opción que desee:");
-        System.out.println("1: AM/FM");
-        System.out.println("2: Siguiente emisora");
-        System.out.println("3: Anterior emisora");
-        System.out.println("4: Guardar emisora");
-        System.out.println("5: Seleccionar una emisora guardada");
-        System.out.println("6: Encender/Apagar la radio");
-        System.out.println("");
+    public void Menu() {
+        Scanner scanner = new Scanner(System.in);
+        Radio radio = new Radio();
+        int button;
+        while (true){
+            System.out.println("");
+            System.out.println("**************************************");
+            System.out.println("                Menú");
+            System.out.println("**************************************");
+            System.out.println("Ingrese la opción que desee:");
+            System.out.println("1: AM/FM");
+            System.out.println("2: Siguiente emisora");
+            System.out.println("3: Anterior emisora");
+            System.out.println("4: Guardar emisora");
+            System.out.println("5: Seleccionar una emisora guardada");
+            System.out.println("6: Encender/Apagar la radio");
+            System.out.println("");
+            
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("AM/FM");
+                    radio.toogleAMFM();
+                    break;
+                case 2:
+                    System.out.println("Siguiente emisora");
+                    radio.nextFrequency();
+                    break;
+                case 3:
+                    System.out.println("Anterior emisora");
+                    radio.previousFrequency();
+                    break;
+                case 4:
+                    System.out.println("Guardar emisora");
+                    System.out.println("Ingrese el número de botón en el cual desea guardar la emisora (del 1 al 12)");
+                    button = scanner.nextInt();
+                    if (button < 1 || button > 12) {
+                        System.out.println("Opción inválida");
+                        break;
+                    }
+                    else {
+                        radio.setFavFrequency(button);
+                        break;
+                    }
+                case 5:
+                    System.out.println("Seleccionar una emisora guardada");
+                    System.out.println("Ingrese el número de botón en el cual desea guardar la emisora (del 1 al 12)");
+                    button = scanner.nextInt();
+                    if (button < 1 || button > 12) {
+                        System.out.println("Opción inválida");
+                        break;
+                    }
+                    else {
+                        radio.getFavFrequency(button);
+                        break;
+                    }
+                case 6:
+                    System.out.println("Encender/Apagar la radio");
+                    radio.tooglePowerOffOn();
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
+        } 
     }
 }
+
